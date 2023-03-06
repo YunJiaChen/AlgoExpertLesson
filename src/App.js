322fe98168138619ffbbb4e803639592a6b952d3
RitaChen <B10202015@gmail.com>
2023-03-06 13:24:55 +0800

    recode App.js&index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Blog posts={posts} /> 
+    </>
+      
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+const posts = [
+  {id:1, title:'Hello React', content:'Welcome to learning React!'},
+  {id:2, title:'Iinstalltion', content:'You can install React from npm.'}
+];
+
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) => 
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+
+  const content = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.content}
+        </li>
+      )}
+    </ul>
+  );
+
+  return (
+    <div>
+      {sidebar}
+      <hr/>
+      {content}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,48 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const posts = [
-  {id:1, title:'Hello React', content:'Welcome to learning React!'},
-  {id:2, title:'Iinstalltion', content:'You can install React from npm.'}
-];
 
-function Blog(props) {
-  const sidebar = (
-    <ul>
-      {props.posts.map((post) => 
-        <li key={post.id}>
-          {post.title}
-        </li>
-      )}
-    </ul>
-  );
-
-  const content = (
-    <ul>
-      {props.posts.map((post) =>
-        <li key={post.id}>
-          {post.content}
-        </li>
-      )}
-    </ul>
-  );
-
-  return (
-    <div>
-      {sidebar}
-      <hr/>
-      {content}
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
-  <Blog posts={posts} />
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
   
 );
 // If you want to start measuring performance in your app, pass a function
